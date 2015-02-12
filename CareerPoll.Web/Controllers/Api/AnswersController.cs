54a3532af9ade4df4e419b4e75fdaaec35d343a4
Scott Wegner <swegner@outlook.com>
2015-02-11 17:41:36 -0800

    Get AnswersController routing working.

--- CareerPoll.Web/Controllers/Api/AnswersController.cs
+++ CareerPoll.Web/Controllers/Api/AnswersController.cs
@@ -1,23 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
+﻿using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Net;
-using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Description;
 using CareerPoll.Web.DataModel;
 
-namespace CareerPoll.Web.Areas.Api.Controllers
+namespace CareerPoll.Web.Controllers.Api
 {
     public class AnswersController : ApiController
     {
         private CareerPollDataModel db = new CareerPollDataModel();
 
-        // GET: api/Answers
+        [HttpGet, Route("api/answers")]
         public IQueryable<Answer> GetAnswers()
         {
             return db.Answers;
