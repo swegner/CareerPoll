25afbe714ff9ea091bc3c8da4b3a9bcdd58dd7b8
Scott Wegner <swegner@outlook.com>
2015-02-14 15:49:51 -0800

    Remove greeting implementation.

--- src/server_test.js
+++ src/server_test.js
@@ -5,18 +5,6 @@ describe('server', function() {
     var server = require("./server.js");
     var http = require("http");
 
-    describe('greeting', function() {
-        var response = server.greeting();
-
-        it('should be friendly', function() {
-            response.should.startWith('hello');
-        });
-
-        it ('should address the world', function(){
-            response.should.endWith('world');
-        });
-    });
-
     describe('start', function() {
         before(function() {
             server.start();
