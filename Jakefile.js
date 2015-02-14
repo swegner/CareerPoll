18e7c07c2f281162b55d0bd38ec1597bedb47404
Scott Wegner <swegner@outlook.com>
2015-02-14 14:23:10 -0800

    lint clean

--- Jakefile.js
+++ Jakefile.js
@@ -1,49 +1,52 @@
-"use strict"
+/*global desc, task, jake, fail, complete */
+(function() {
+    "use strict";
 
-desc("Do all the things")
-task("default", ["lint"], function() {
-	console.log("done!");
-})
+    desc("Do all the things");
+    task("default", ["lint"], function() {
+        console.log("done!");
+    });
 
-desc("Lint everything")
-task("lint", [], function() {
-	console.log("linting..");
+    desc("Lint everything");
+    task("lint", [], function() {
+        console.log("linting..");
 
-	var lint = require("jake-jshint");
-	var jake = require("jake");
+        var lint = require("jake-jshint");
+        var jake = require("jake");
 
-	var files = new jake.FileList();
-	files.include("./*.js");
+        var files = new jake.FileList();
+        files.include("./*.js");
 
-	var config = getLintConfig();
-	var pass = lint.validateFileList(files.toArray(), config.options, config.globals);
-	if (!pass) fail("Lint failed");
-});
+        var config = getLintConfig();
+        var pass = lint.validateFileList(files.toArray(), config.options, config.globals);
+        if (!pass) fail("Lint failed");
+    });
 
-function getLintConfig() {
-	return {
-		options: {
-		    bitwise: true,
-		    curly: false,
-		    eqeqeq: true,
-		    forin: true,
-		    immed: true,
-		    latedef: true,
-		    newcap: true,
-		    noarg: true,
-		    noempty: true,
-		    nonew: true,
-		    regexp: true,
-		    undef: true,
-		    strict: true,
-		    trailing: true,
-		    node: true
-	  }, 
-	  globals: {
-	    describe: false,
-	    it: false,
-	    beforeEach: false,
-	    afterEach: false
-	  }
-	}
-}
+    function getLintConfig() {
+        return {
+            options: {
+                bitwise: true,
+                curly: false,
+                eqeqeq: true,
+                forin: true,
+                immed: true,
+                latedef: true,
+                newcap: true,
+                noarg: true,
+                noempty: true,
+                nonew: true,
+                regexp: true,
+                undef: true,
+                strict: true,
+                trailing: true,
+                node: true
+            },
+            globals: {
+                describe: false,
+                it: false,
+                beforeEach: false,
+                afterEach: false
+            }
+        };
+    }
+}());
